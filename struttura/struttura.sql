632483fe3213a7051c1e2a008e6a6ac66e08d6f1
Salemme02ewfw <samuele.verna@student.univaq.it>
2023-06-30 16:25:28 +0200

    revisione struttura

--- struttura/struttura.sql
+++ struttura/struttura.sql
@@ -20,14 +20,14 @@ create table collezione (
 );
 
 create table genere (
-	nome varchar(50) primary key
+	nome varchar(50) primary key 
 );
 
 create table disco (
 	ID int unsigned primary key auto_increment,
     titolo varchar(50) not null,
     etichetta varchar(50) not null,
-    annoUscita smallint unsigned not null,
+    annoUscita tinyint unsigned not null,
     nomeGenere varchar(50) not null,
     ID_collezione int unsigned not null,
     foreign key (nomeGenere) references genere(nome) on delete restrict on update cascade,
@@ -42,13 +42,14 @@ create table formato (
 
 create table copia (
 	ID int unsigned primary key auto_increment,
-    numeroBarcode tinyint unsigned not null,
+    numeroBarcode tinyint unsigned,
     numeroCopia int unsigned,
     nomeFormato varchar(50) not null,
+    statoDiConservazione enum('nuovo','eccellente','buono','discreto','rovinato'),
     ID_disco int unsigned not null,
     foreign key (nomeFormato) references formato(nome) on delete restrict on update cascade,
     foreign key (ID_disco) references disco(ID) on delete cascade on update cascade,
-    unique(numeroBarcode, ID_disco)
+    unique(numeroCopia, ID_disco)
 );
 
 create table posizione (
@@ -57,15 +58,15 @@ create table posizione (
 
 create table immagine (
 	ID int unsigned primary key auto_increment,
-    url varchar(1000) not null,
-    dimensione smallint unsigned,
-    formato varchar(50),
-    risoluzione int unsigned,
+    url varchar(767) not null,
+    dimensione varchar(9),
+    formato enum('PNG', 'JPEG', 'GIF', 'SVG'),
     nomePosizione varchar(50),
     ID_copia int unsigned not null,
     foreign key (nomePosizione) references posizione(nome) on delete restrict on update cascade,
     foreign key (ID_copia) references copia(ID) on delete cascade on update cascade,
-    unique(ID_copia)
+    unique(url),
+    check(dimensione like '%x%')
 );
 
 create table traccia (
@@ -85,31 +86,33 @@ create table artista (
 );
 
 create table ruolo (
-	nome varchar(50) primary key,
-    check (nome in ('compositore', 'cantante', 'musicista'))
+	nome varchar(50) primary key
 );
 
 create table condivisa (
-	ID_collezione int unsigned primary key,
-    ID_collezionista int unsigned primary key,
+	ID_collezione int unsigned,
+    ID_collezionista int unsigned,
+    primary key(ID_collezione, ID_Collezionista),
     foreign key (ID_collezione) references collezione(ID) on delete cascade on update cascade,
     foreign key (ID_collezionista) references collezionista(ID) on delete cascade on update cascade
 );
 
 create table autore (
-	ID_disco int unsigned primary key,
-    ID_artista int unsigned primary key,
+	ID_disco int unsigned,
+    ID_artista int unsigned,
     nomeRuolo varchar(50) not null,
+    primary key(ID_disco, ID_artista),
     foreign key (ID_disco) references disco(ID) on delete cascade on update cascade,
     foreign key (ID_artista) references artista(ID) on delete cascade on update cascade,
-    foreign key (nomeRuolo) references ruolo(nome) on delete restrict on update restrict
+    foreign key (nomeRuolo) references ruolo(nome) on delete restrict on update cascade
 );
 
 create table collaborazione (
-	ID_traccia int unsigned primary key,
-    ID_artista int unsigned primary key,
+	ID_traccia int unsigned,
+    ID_artista int unsigned,
     nomeRuolo varchar(50) not null,
+    primary key(ID_traccia, ID_artista),
     foreign key (ID_traccia) references traccia(ID) on delete cascade on update cascade,
     foreign key (ID_artista) references artista(ID) on delete cascade on update cascade,
-    foreign key (nomeRuolo) references ruolo(nome) on delete restrict on update restrict
+    foreign key (nomeRuolo) references ruolo(nome) on delete restrict on update cascade
 );
